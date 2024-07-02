6cc484eb9b0fc42f451904fdd9951406abdaceca
d3nari <dungerway@gmail.com>
2024-07-02 19:02:55 +0300

    Start Work

---
+++ Dictionary/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Text;
+
+const string PathToFile = "../../../Vocabulary.txt";
+
+Console.WriteLine( "The Dictionary" );
+
+
+
+Console.Write( "Введите слово(на английском или русском языке): " );
+string Word = Console.ReadLine();
+
+if ( IsWordExistsInDictionary( Word ) )
+{
+    Console.WriteLine( "We Found Your Word!" );
+}
+else
+{
+    //слова нет в словаре
+}
+//поиск слова в словаре
+//если слово не найдено - предложить пользователю добавить его в словарь
+//если слово есть - то выводим его перевод. 
+
+bool IsWordExistsInDictionary( string SearchWord )
+{
+    string TextFromFile = ReadTextFromFile();
+    if ( TextFromFile.ToLower().Contains( SearchWord ) || TextFromFile.ToUpper().Contains( SearchWord ) )
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+string ReadTextFromFile()
+{
+    using ( StreamReader reader = new StreamReader( PathToFile, Encoding.ASCII ) )
+    {
+        string text = reader.ReadToEnd();
+        return text;
+
+    }
+}
